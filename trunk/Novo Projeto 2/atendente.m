ed90fcfe9333b8da86946cb80725e091c455d214
lluizfernandotrindade@gmail.com <lluizfernandotrindade@gmail.com@5114ad47-e230-cb71-f657-abe213d759bd>
2013-12-12 21:10:14 +0000

    Código o inicio

---
+++ trunk/Novo Projeto 2/atendente.m
@@ -0,0 +1,13 @@
+classdef atendente
+    %ATENDENTE Summary of this class goes here
+    %   Detailed explanation goes here
+    properties
+        codat;
+        nomeat;
+    end
+    
+    methods
+    end
+    
+end
+
